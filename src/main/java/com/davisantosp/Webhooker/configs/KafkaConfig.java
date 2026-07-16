5b7e97735ac1a13c9b608a7c6d8ce687d5a605f1
Davi Santos <davisantosp.dev@gmail.com>
2026-07-16 12:17:34 -0300

    fix: Kafka's replication factor bug fix that caused malfunction on Consumer

--- src/main/java/com/davisantosp/Webhooker/configs/KafkaConfig.java
+++ src/main/java/com/davisantosp/Webhooker/configs/KafkaConfig.java
@@ -1,6 +1,7 @@
 package com.davisantosp.Webhooker.configs;
 
 import com.davisantosp.Webhooker.domain.DTOs.EventRequestDTO;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -8,7 +9,9 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
 import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
@@ -17,10 +20,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class KafkaConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
+
+    @Bean
+    public NewTopic webhookerEventsTopic() {
+        return TopicBuilder.name("webhooker-events")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
 
     @Bean
     public ProducerFactory<String, EventRequestDTO> producerFactory() {
@@ -45,7 +57,6 @@ public class KafkaConfig {
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JacksonJsonDeserializer.class);
         configProps.put(JacksonJsonDeserializer.TRUSTED_PACKAGES, "*");
-        configProps.put(JacksonJsonDeserializer.USE_TYPE_INFO_HEADERS, false);
         configProps.put(JacksonJsonDeserializer.VALUE_DEFAULT_TYPE, EventRequestDTO.class.getName());
         return new DefaultKafkaConsumerFactory<>(configProps);
     }
