a63bbf93d19b80943fc5e1561d9376e07688955a
Davi Santos <davisantosp.dev@gmail.com>
2026-07-20 13:57:03 -0300

    feat: Basic Consumer implementation for Kafka and Producer refactoring fixing asynchronous issue

--- src/main/java/com/davisantosp/Webhooker/repositories/RuleRepository.java
+++ src/main/java/com/davisantosp/Webhooker/repositories/RuleRepository.java
@@ -4,8 +4,10 @@ import com.davisantosp.Webhooker.domain.entities.Rule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface RuleRepository extends JpaRepository<Rule, UUID> {
+    List<Rule> findByUserIdAndEventTypeAndActiveTrue(String userId, String eventType);
 }
--- src/main/java/com/davisantosp/Webhooker/services/EventProducerService.java
+++ src/main/java/com/davisantosp/Webhooker/services/EventProducerService.java
@@ -1,12 +1,9 @@
 package com.davisantosp.Webhooker.services;
 
 import com.davisantosp.Webhooker.domain.DTOs.EventRequestDTO;
-import com.davisantosp.Webhooker.infra.exceptions.ServiceNotAvailableException;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.TimeUnit;
 
 @Service
 public class EventProducerService {
@@ -18,12 +15,11 @@ public class EventProducerService {
     }
 
     public void addEvent(EventRequestDTO eventRequestDTO){
-        try {
-            kafkaTemplate.send("webhooker-events", eventRequestDTO.userId(), eventRequestDTO)
-                    .get(3, TimeUnit.SECONDS);
-
-        } catch (Exception e) {
-            throw new ServiceNotAvailableException("Messager temporarily unavailable. Try again later.");
-        }
+        kafkaTemplate.send("webhooker-events", eventRequestDTO.userId(), eventRequestDTO)
+                .whenComplete((result, ex) -> {
+                    if (ex != null) {
+                        System.err.println("Failed to send event to Kafka: " + ex.getMessage());
+                    }
+                });
     }
 }
